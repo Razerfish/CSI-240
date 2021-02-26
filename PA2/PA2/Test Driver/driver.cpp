98c6d3a543784a2afff0833a9a318ef3c76f04e8
Fiona Wilson <fiona@razerfish.dev>
2021-02-26 16:58:09 -0500

    First iteration of driver.cpp

--- PA2/PA2/Test Driver/driver.cpp
+++ PA2/PA2/Test Driver/driver.cpp
@@ -13,3 +13,36 @@
 // - Communicate a copy of this assignment to a plagiarism checking service
 // (which may then retain a copy of this assignment on its database for
 // the purpose of future plagiarism checking)
+
+#include <iostream>
+#include <string>
+
+#include "vendingMachine.h"
+#include "header.h"
+
+using namespace std;
+int main()
+{
+	VendingMachine test;
+
+	cout
+		<< test.getBalance() << endl
+		<< test.getItemCount() << endl
+		<< test.getName() << endl
+		<< test.getPassword() << endl << endl;
+
+	Item x;
+	for (int i = 0; i < 4; i++)
+	{
+		x = test.getItem(i);
+		cout << x.name << " " << x.quantity << " " << x.price << endl;
+	}
+
+	test.setBalance(42.00);
+	test.setItem(4, { "egg", 2, 3.00 });
+	test.setItemCount(5);
+	test.setName("Barry");
+	test.setPassword("beans");
+
+	return 0;
+}
