1f6492ca67e1a690aa60c3ab9f6de1828178f8ba
Fiona Wilson <fiona@razerfish.dev>
2021-03-12 15:46:40 -0500

    Add header information to lab4.cpp header.h and functions.cpp

---
+++ Lab4/Lab4/lab4.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Fiona Wilson
+Class: CSI-240-03
+Assignment: Lab 4
+Date Assigned: March 8 2021
+Due Date: March 15 2021
+Description: Create a dynamic array of Student objects.
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given fully documented references to the work of others. I understand the definition and
+consequences of plagiarism and acknowledge that the assessor of this assignment
+may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking service
+(which may then retain a copy of this assignment on its database for
+the purpose of future plagiarism checking)
+*/
