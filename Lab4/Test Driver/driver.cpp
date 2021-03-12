7a54f8f7cdbf7d7dc555056067227bcc2d2cd2b8
Fiona Wilson <fiona@razerfish.dev>
2021-03-12 16:30:08 -0500

    Create test driver

---
+++ Lab4/Test Driver/driver.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include "Lab4/student.h"
+#include "Lab4/header.h"
+
+using namespace std;
+
+const string DATA_FILE = "../Lab4/data.txt";
+
+int main()
+{
+	return 0;
+}
