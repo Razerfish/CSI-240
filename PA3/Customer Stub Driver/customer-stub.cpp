5346c5ab56f109ed4c606248e8d0081a3c3aea4f
Fiona Wilson <fiona@razerfish.dev>
2021-03-28 13:20:52 -0400

    Create the Customer Stub Driver Project

---
+++ PA3/Customer Stub Driver/customer-stub.cpp
@@ -0,0 +1,15 @@
+// Author: Fiona Wilson
+// Class: CSI-240-03
+// Assignment: PA3 - Part 2
+// Date Assigned: March 22 2021
+// Due Date: March 30 2021
+// Description: Create an account class and make a stub driver to demonstrate it.
+// Certification of Authenticity:
+// I certify that this is entirely my own work, except where I have given fully documented references to the work of others. I understand the definition and
+// consequences of plagiarism and acknowledge that the assessor of this assignment
+// may, for the purpose of assessing this assignment:
+// - Reproduce this assignment and provide a copy to another member of
+// academic staff; and/or
+// - Communicate a copy of this assignment to a plagiarism checking service
+// (which may then retain a copy of this assignment on its database for
+// the purpose of future plagiarism checking)
