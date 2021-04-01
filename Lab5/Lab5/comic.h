411dd2236c8a8fd3d6bcfc4c8c32d772f784e541
Fiona Wilson <fiona@razerfish.dev>
2021-04-01 13:11:29 -0400

    Create files

---
+++ Lab5/Lab5/comic.h
@@ -0,0 +1,22 @@
+/*
+Author: Fiona Wilson
+Class: CSI-240-03
+Assignment: Lab 5
+Date Assigned: March 29 2021
+Due Date: April 1 2021
+Description: Create an item class, a book class that inherits from item and a comic class that inherits from book. Write a stub driver to test them all.
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given fully documented references to the work of others. I understand the definition and
+consequences of plagiarism and acknowledge that the assessor of this assignment
+may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+- Communicate a copy of this assignment to a plagiarism checking service
+(which may then retain a copy of this assignment on its database for
+the purpose of future plagiarism checking)
+*/
+
+#ifndef _COMIC_H
+#define _COMIC_H
+
+#endif
